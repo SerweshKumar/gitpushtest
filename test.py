645eeee6442a107e6996a60b4ec1cfe4151881c3
srwshhv2@gmail.com <Ser@1994>
2022-08-21 17:42:14 +0530

    I am testing Gir COmmit and push from pycharm.

---
+++ test.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://srwshhv2:Mongodb123@cluster0.4zdaegu.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d={
+    "name":"Serwesh",
+    "enail":"abc@gmail.com",
+    "surname":"kumar"
+}
+db1=client['mongodbtest']
+coll=db1['test']
+coll.insert_one(d)
